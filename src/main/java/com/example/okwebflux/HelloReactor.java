d5757c9dd03a7a6aabdfc62c97ef7f04180e911b
신희성 <vvwv3092@gmail.com>
2024-09-26 13:59:53 +0900

    update: reactor simple

--- src/main/java/com/example/okwebflux/HelloReactor.java
+++ src/main/java/com/example/okwebflux/HelloReactor.java
@@ -1,10 +1,15 @@
 package com.example.okwebflux;
 
-import reactor.core.publisher.Mono;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 
+@Slf4j
 public class HelloReactor {
     public static void main(String[] args) {
-        Mono.just("Hello, Reactor")
-                .subscribe(System.out::println);
+        Flux<String> sequence = Flux.just("Hello", "Reactor");
+
+        sequence
+                .map(data -> data.toLowerCase())
+                .subscribe(data -> log.info(data));
     }
 }
